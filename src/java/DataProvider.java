af1b4dd03a04a40bbe2c252fdedcf9666a4d646b
manoj <manojs121@rediffmail.com>
2020-08-17 18:31:33 +0530

    17-08

--- src/java/DataProvider.java
+++ src/java/DataProvider.java
@@ -37,10 +37,30 @@ public class DataProvider extends HttpServlet {
         //provides the response
             
             PrintWriter out=response.getWriter();
-            out.println("Thanks For Visiting Us.");
-            out.println("Total Cases In "+country+" : "+totalCases);
-            out.println("Active Cases In "+country+" : "+activeCases);
-            out.println("Total Deaths Cases In "+country+" : "+totalDeath);
+         
+            out.println("<html>");
+            out.println("<body bgcolor=\"pink\">");
+            out.println("<h3>Welcome To Our Site</h3>");
+            out.println("<h4>Informative Portal For Covid Information</h4>");
+            out.println("<hr>");
+            out.println("<table border=\"2\">");
+            out.println("<tr>");
+            out.println("<td>Total Cases</td>");
+            out.println("<td>"+totalCases+"</td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td>Active Cases</td>");
+            out.println("<td>"+activeCases+"</td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td>Total Deaths</td>");
+            out.println("<td>"+totalDeath+"</td>");
+            out.println("</tr>");
+            out.println("</table>");
+            out.println("<hr>");
+            out.println("<marquee>Stay At Home, Be Safe</marquee>");
+            out.println("</body></html>");
+            
             
             out.close();
     }
