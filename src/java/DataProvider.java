a1c2742f0d26f95321e2b1f86cecaa56150f1d5a
manoj <manojs121@rediffmail.com>
2020-08-19 18:39:39 +0530

    19-08

--- src/java/DataProvider.java
+++ src/java/DataProvider.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -6,63 +7,74 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DataProvider extends HttpServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //reads the request
-            String country=request.getParameter("t1");
+        String color=request.getParameter("bgclr");
+        String name = request.getParameter("name");
+        String country = request.getParameter("country");
+        String items[] = request.getParameterValues("donation");
         //process the request
-            int totalCases=0;
-            int activeCases=0;
-            int totalDeath=0;
-            
-            switch(country){
-                case "india":
-                    totalCases=2000000;
-                    activeCases=500000;
-                    totalDeath=29000;
+        int totalCases = 0;
+        int activeCases = 0;
+        int totalDeath = 0;
+
+        switch (country) {
+            case "india":
+                totalCases = 2000000;
+                activeCases = 500000;
+                totalDeath = 29000;
                 break;
-                case "usa":
-                    totalCases=2500000;
-                    activeCases=700000;
-                    totalDeath=39000;
+            case "usa":
+                totalCases = 2500000;
+                activeCases = 700000;
+                totalDeath = 39000;
                 break;
-                case "uk":
-                    totalCases=800000;
-                    activeCases=300000;
-                    totalDeath=129000;
+            case "uk":
+                totalCases = 800000;
+                activeCases = 300000;
+                totalDeath = 129000;
                 break;
-            }
-        
+        }
+
         //provides the response
-            
-            PrintWriter out=response.getWriter();
-         
-            out.println("<html>");
-            out.println("<body bgcolor=\"pink\">");
-            out.println("<h3>Welcome To Our Site</h3>");
-            out.println("<h4>Informative Portal For Covid Information</h4>");
-            out.println("<hr>");
-            out.println("<table border=\"2\">");
-            out.println("<tr>");
-            out.println("<td>Total Cases</td>");
-            out.println("<td>"+totalCases+"</td>");
-            out.println("</tr>");
-            out.println("<tr>");
-            out.println("<td>Active Cases</td>");
-            out.println("<td>"+activeCases+"</td>");
-            out.println("</tr>");
-            out.println("<tr>");
-            out.println("<td>Total Deaths</td>");
-            out.println("<td>"+totalDeath+"</td>");
-            out.println("</tr>");
-            out.println("</table>");
-            out.println("<hr>");
-            out.println("<marquee>Stay At Home, Be Safe</marquee>");
-            out.println("</body></html>");
-            
-            
-            out.close();
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<body bgcolor="+color+">");
+        out.println("<h3>Welcome " + name + " To Our Site</h3>");
+        out.println("<h4>Information For " + country + "</h4>");
+        out.println("<hr>");
+        out.println("<table border=\"2\">");
+        out.println("<tr>");
+        out.println("<td>Total Cases</td>");
+        out.println("<td>" + totalCases + "</td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td>Active Cases</td>");
+        out.println("<td>" + activeCases + "</td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<td>Total Deaths</td>");
+        out.println("<td>" + totalDeath + "</td>");
+        out.println("</tr>");
+        out.println("</table>");
+        if (items != null) {
+            out.println("<h4>You Want To Donate : </h4>");
+            out.println("<ul>");
+            for (String item : items) {
+                out.println("<li>");
+                out.println(item);
+                out.println("</li>");
+            }
+            out.println("</ul>");
+        }
+        out.println("<hr>");
+        out.println("<marquee>Stay At Home, Be Safe</marquee>");
+        out.println("</body></html>");
+
+        out.close();
     }
-    
+
 }
--- web/input.jsp
+++ web/input.jsp
@@ -4,8 +4,30 @@
         <h4>Please-Provide-Country-Name</h4>
         <hr>
         <form action="DataProvider" method="get">
-            Country-Name    <input type="text" name="t1"/>
-            <input type="submit" value="Get-Data"/>
+            <pre>
+            Output-Bkgrnd   pink <input type="radio" name="bgclr" value="pink" checked="check"/> gray <input type="radio" name="bgclr" value="gray"/>
+            Your-Name       <input type="text" name="name"/>
+            Your-Picture    <input type="file" name="pic"/>
+            Country-Name    <select name="country">
+                                <option>india</option>
+                                <option>usa</option>
+                                <option>uk</option>
+                            </select>
+            
+            Donation        mask    <input type="checkbox" name="donation" value="mask"/>
+                            ppe     <input type="checkbox" name="donation" value="ppe"/>
+                            vaccine <input type="checkbox" name="donation" value="vaccine"/>
+            <!--                
+            Donation        <select name="donation" multiple="multiple">
+                                <option selected="true">mask</option>
+                                <option>ppe</option>
+                                <option>vaccine</option>
+                                <option>medicines</option>
+                                <option>ambulence</option>
+                            </select>
+            -->
+                            <input type="submit" value="Get-Data"/>
+            </pre>
         </form>
         <hr>
     </body>
